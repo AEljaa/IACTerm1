7992cff0cbe929ba083f2de4b004db1d29373ddf
AEljaa <adamtantalum@gmail.com>
2023-11-08 00:59:12 +0000

    Update

--- Lab1-Counter/task3/counter_tb.cpp
+++ Lab1-Counter/task3/counter_tb.cpp
@@ -1,56 +1,56 @@
-#include "Vcounter.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
-#include "vbuddy.cpp"
-int main(int argc,char **argv, char **env){
-    int i;
-    int clk;
+#include "Vsinegen.h"
 
-    Verilated::commandArgs(argc, argv);
-    //init top verilog instance
-    Vcounter *top = new Vcounter;
-    //init trace dump
+#include "vbuddy.cpp"     // include vbuddy code
+#define MAX_SIM_CYC 1000000
+#define ADDRESS_WIDTH 8
+#define ROM_SZ 256
 
-    Verilated::traceEverOn(true);
-    VerilatedVcdC* tfp=new VerilatedVcdC;
-    top->trace (tfp,99);
-    tfp->open ("Vcounter.vcd");
+int main(int argc, char **argv, char **env) {
+  int simcyc;     // simulation clock count
+  int tick;       // each clk cycle has two ticks for two edges
 
-    if(vbdOpen() != 1) return(-1);
-    
-    vbdHeader("Lab 1: Counter");
-    top->clk=1;
-    top->rst=1;
-    top->ld=0;
-    top->v=0;
+  Verilated::commandArgs(argc, argv);
+  // init top verilog instance
+  Vsinegen* top = new Vsinegen;
+  // init trace dump
+  Verilated::traceEverOn(true);
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+  top->trace (tfp, 99);
+  tfp->open ("sinegen.vcd");
+ 
+  // init Vbuddy
+  if (vbdOpen()!=1) return(-1);
+  vbdHeader("L2T1: SigGen");
+  //vbdSetMode(1);        // Flag mode set to one-shot
 
-    vbdSetMode(1);
-    for (i=0; i<10000; i++){
+  // initialize simulation inputs
+  top->clk = 1;
+  top->rst = 0;
+  top->en = 1;
+  top->incr = 1;
 
-        for (clk =0; clk<2; clk++){
-            tfp->dump (2*i +clk);
-            top->clk= !top->clk;
-            top->eval ();
-
-        }
-        
-        vbdPlot(int(top->count), 0, 255);
-        vbdCycle(i+1);
-        
-        top->rst = (i<2) | (i==15); // condition that will cause rst to = 1
-        if(vbdFlag()){
-            top->ld=1;
-            top->v=vbdValue();
-        }
-        else {
-            top->ld=0;
-        }
-        if (Verilated::gotFinish()) exit(0);
-
+  // run simulation for MAX_SIM_CYC clock cycles
+  for (simcyc=0; simcyc<MAX_SIM_CYC; simcyc++) {
+    // dump variables into VCD file and toggle clock
+    for (tick=0; tick<2; tick++) {
+      tfp->dump (2*simcyc+tick);
+      top->clk = !top->clk;
+      top->eval ();
     }
     
-    vbdClose(); 
-    tfp->close();
-    exit(0);
+    top->incr = vbdValue();
+    // plot ROM output and print cycle count
+    vbdPlot(int (top->dout), 0, 255);
+    vbdCycle(simcyc);
 
+    // either simulation finished, or 'q' is pressed
+    if ((Verilated::gotFinish()) || (vbdGetkey()=='q')) 
+      exit(0);                // ... exit if finish OR 'q' pressed
+  }
+
+  vbdClose();     // ++++
+  tfp->close(); 
+  exit(0);
 }
